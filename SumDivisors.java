be6a3159f1718d4df9f6ddaa816eea511979c156
evan999 <emeshberge@gmail.com>
2020-03-18 21:26:25 -0400

    Sum of all divisors a given integer. Passes all tests.

---
+++ SumDivisors.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        int num = 1;
+        int sum = 0;
+
+        while(num <= n)
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+
+            if(n % num == 0)
+            {
+                sum += num;
+            }
+            num++;
+        }
+
+        return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
