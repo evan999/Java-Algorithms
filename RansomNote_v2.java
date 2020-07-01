3545ce00f4ce0a20015a893626c20b2618f84258
evan999 <emeshberge@gmail.com>
2020-06-30 22:52:11 -0400

    Ransom Note v2.

---
+++ RansomNote_v2.java
@@ -0,0 +1,16 @@
+/*
+        for(int i = 0; i < note.length; i++) {
+            // Remove words that don't appear in magazine
+            if(!magazineWords.containsKey(note[i])){
+                System.out.println("No");
+                return;
+            }
+            if(magazineWords.get(note[i]) == 0){
+                System.out.println("No");
+                return;
+            }
+            // Reduce count by 1 in 
+            //magazineWords.set(note[i], magWords.get(note[i])-1)
+
+        }
+        */
