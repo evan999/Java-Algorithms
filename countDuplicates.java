1127f26c9d6c74dd4633bab1f413a0fb970dd893
evan999 <emeshberge@gmail.com>
2020-07-22 22:58:22 -0400

    CountDuplicates problem solution - outputs the number of duplicate elements in list. Compiles and all test cases pass.

---
+++ countDuplicates.java
@@ -0,0 +1,34 @@
+class Result {
+
+    public static int countDuplicate(List<Integer> numbers) {
+        
+        Collections.sort(numbers);
+
+        HashMap<Integer, Integer> listNumbers = new HashMap<Integer, Integer>(); 
+
+        int dupes = 0;
+        
+        
+        for(int i = 0; i < numbers.size(); i++){
+            if(!listNumbers.containsKey(numbers.get(i))){
+                listNumbers.put(numbers.get(i), 1);
+            } 
+            else {
+                listNumbers.put(numbers.get(i), numbers.get(i)+1); 
+            }
+        }
+
+        for(Map.Entry number : listNumbers.entrySet()){
+            int occurrences = (int)number.getValue();
+            
+            if(occurrences > 1){
+                dupes++;
+            }
+            else {
+                continue;
+            }
+				}
+        return dupes;
+    }
+
+}
