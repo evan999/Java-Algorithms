c5e60d5bb0492068f5e6f0de5ffc114194b61ef6
evan999 <emeshberge@gmail.com>
2020-08-26 22:12:39 -0400

    Solution in progress. There's a bug that needs to be fixed, which has to do with the indexing. It's not currently reaching all elements in the array. Fixing this bug will likely solve the issue and pass all tests.

---
+++ newYearsChaos.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+
+        int minBribes = 0;
+        boolean chaotic = false;
+        int bribes = 0;
+
+        for(int position = 0; position < q.length; position++)
+        {
+            //int bribes = 0;
+            // Keep track of how many times each person moves.
+            // Each move = 1 bribe
+            //System.out.println(q[position-1]);
+            int currentValue = q[position];
+            int currentPosition = position+1;
+            //System.out.println(currentValue);
+            
+            if(currentPosition!=q[position]){
+                int difference = 0;
+                if(q[position]>currentPosition){
+                    difference = q[position]-currentPosition;
+                }
+
+                if(difference > 2){
+                    chaotic = true;
+                    break;
+                }
+                bribes += difference;
+            }
+            else{
+                continue;
+            }
+
+            minBribes = bribes;
+        }
+
+        if(chaotic==false){
+            System.out.println(minBribes);
+        }
+        else{
+            System.out.println("Too chaotic");
+        }
+    
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
